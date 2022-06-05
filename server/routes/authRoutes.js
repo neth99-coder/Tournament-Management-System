c77140dc1f806fea379f2821409825761b36db1f
ThaminduSathruwan <sathruwan99@gmail.com>
2022-06-05 21:43:34 +0530

    Remove ignored files

--- server/routes/authRoutes.js
+++
@@ -1,6 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const authController = require("../controllers/authController");
-router.post("/login", authController.loginUser);
-router.post("/signup", authController.signupUser);
-module.exports = router;
--- server/routes/playerRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const playerController = require("../controllers/playerController");
-
-router.get("/getProfile/:playerID",playerController.getProfile);
-router.get("/tournaments/:playerID",playerController.getRegisteredTournaments);
-router.put("/updateProfile",playerController.updateProfile);
-router.post("/confirmPasswords",playerController.confirmPasswords);
-module.exports=router;           
